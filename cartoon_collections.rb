a01c0569e213ae62c93003d1f011ac2dbbb1afd8
John Shelby <jsnd11@gmail.com>
2016-12-30 11:19:41 -0500

    cartoonish

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_array)
+  dwarf_array.each_with_index {|dwarf,index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planet_array)
+  planet_array.collect {|planet| planet.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
+  calls.any? {|call| call.length>4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(could_you_be_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  could_you_be_cheese.find {|item| cheese_types.include?(item)}
 end
